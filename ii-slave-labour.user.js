aafe19b310b56b6397ed467057e763e584c1b682
RedHatter <_c_@mail.com>
2014-04-22 22:30:34 -0700

    ii-slave-labor loads before page.

--- ii-slave-labour.user.js
+++ ii-slave-labour.user.js
@@ -5,6 +5,8 @@
 // @include     http://*improbableisland.com/*
 // @version     1.0
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @run-at      document-start
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_deleteValue
@@ -13,45 +15,115 @@
 
 var actions = [
 	// Name, Conditional link, Path
-	["Refresh booze", "Return to Squat Hole",
-		["Return to Squat Hole", "Booz", "Chlamydia", "Mudwisearse", "Return to the lounge", "Chlamydia", "Wanker", "Back to Squat Hole", "Jungle"]
-	],
-	["Refresh booze", "Return to Improbable Central",
-		["Return to Improbable Central", "The Prancing SpiderKitty", "Talk to Dan the Landlord", "Ale", "Return to Improbable Central", "Jungle"]
-	],
-	["Deposit Req", "Return to",
-		["Return to", "Bank of Improbable", "Deposit", function () {$('input[type=submit]').click();}, "Return to", "Jungle"]
-	]
+	{
+		name: "Free Stuff",
+		conditional: "Joe's Diner",
+		steps: ["Council Offices", "Claim your Free Stuff", "BANG Grenade", "BANG Grenade", "ZAP Grenade", "Leave"]
+	},
+	{
+		name: "Heal",
+		conditional: "Jungle",
+		steps: ["Jungle", "Hospital Tent", "Complete Healing", "Return to"]
+	},
+	{
+		name: "Heal",
+		conditional: "Hospital Tent",
+		steps: ["Hospital Tent", "Complete Healing"]
+	},
+	{
+		name: "Refresh booze",
+		conditional: "Return to Squat Hole",
+		steps: ["Return to Squat Hole", "Booz", "Chlamydia", "Mudwisearse", "Return to the lounge", "Chlamydia", "Wanker", "Back to Squat Hole", "Jungle"]
+	},
+	{
+		name: "Refresh booze",
+		conditional: "Return to Improbable Central",
+		steps: ["Return to Improbable Central", "The Prancing SpiderKitty", "Talk to Dan the Landlord", "Ale", "Return to Improbable Central", "Jungle"]
+	},
+	{
+		name: "Deposit Req",
+		conditional: "Return to",
+		steps: ["Return to", "Bank of Improbable", "Deposit", function () {$('input[type=submit]').click();}, "Return to", "Jungle"]
+	},
+	{
+		name: "Deposit Req",
+		conditional: "Bank of Improbable",
+		steps: ["Bank of Improbable", "Deposit", function () {$('input[type=submit]').click();}, "Return to"]
+	}
 ];
 
 var auto = [
 	// Conditional link
-//	"Pick up SneezeRoot",
-//	"Pick up TwitchLeaf",
+	// "Pick up all Snarls of SneezeRoot",
+	// "Pick up SneezeRoot",
+	// "Pick up all Clumps of TwitchLeaf",
+	// "Pick up TwitchLeaf",
+	// "Pick up all Pods of SteelSeed",
+	// "Pick up SteelSeed",
+	// "Pick up all Supply Crates",
+	// "Pick up Supply Crate",
 ]
 
-actions.forEach(function(action, index)
+var insert = null;
+waitForKeyElements (".navhead", function (node)
 {
-	if ($('a:contains("'+action[1]+'")').length > 0)
-		GM_registerMenuCommand(action[0], function ()
-		{
-			GM_setValue('action', index);
-			GM_setValue('step', 0);
-			window.location = window.location;
-		});
+	if (insert == null)
+	{
+		insert = node;
+	}
 });
 
-for (let action of auto)
+var header = false;
+waitForKeyElements ("a.nav", function (node)
 {
-	var link = $('a:contains("'+action+'")');
-	if (link.length > 0)
-		window.location.href = link.attr('href');
-}
+	if ($(node).attr('href') == undefined)
+		return;
+	else
+		console.log ($(node).attr('href'));
 
-traverse();
+	for (let action of auto)
+	{
+		if (node.text().indexOf(action) > -1)
+		{
+			window.location.href = node.attr('href');
+			break;
+		}
+	}
 
-function traverse()
+	actions.forEach(function(action, index)
+	{
+		if ((typeof action.conditional == 'string' && node.text().indexOf(action.conditional) > -1)
+			|| (typeof action.conditional == 'function' && action.conditional()))
+		{
+			GM_registerMenuCommand(action.name, function ()
+			{
+				GM_setValue('action', index);
+				GM_setValue('step', 0);
+				window.location = window.location;
+			});
+
+			if (!header)
+			{
+				$("<div class='navhead'>Slaves</div>").insertBefore (insert);
+				insert = $("<br clear=all>").insertBefore (insert)
+				header = true;
+			}
+
+			$("<a class='nav slave'>"+action.name+"</a>").click (function ()
+			{
+					GM_setValue('action', index);
+					GM_setValue('step', 0);
+					traverse (node);
+			}).insertBefore (insert);
+		}
+	});
+
+	traverse (node);
+});
+
+function traverse (node)
 {
+	// Do the current action
 	var index = GM_getValue('action');
 
 	if (typeof index == 'undefined')
@@ -59,21 +131,34 @@ function traverse()
 
 	var action = actions[index];
 	var step = GM_getValue('step');
-	if (action[2].length > step)
-		GM_setValue('step', step+1);
-	else
-	{
-		GM_deleteValue('step');
-		GM_deleteValue('action');
-	}
 
-	if (typeof action[2][step] == 'string')
+	if (typeof action.steps[step] == 'string')
 	{
-		var url = $('a:contains("'+action[2][step]+'")').attr('href');
-		if (typeof url != 'undefined')
-			window.location.href = url;
+		if ( node.text().indexOf(action.steps[step]) > -1
+			&& typeof node.attr('href') != 'undefined')
+		{
+			window.location.href = node.attr('href');
+			if (action.steps.length > step+1)
+				GM_setValue('step', step+1);
+			else
+			{
+				GM_deleteValue('step');
+				GM_deleteValue('action');
+			}
+		}
 	}
 	else
-		action[2][step]();
+	{
+		$(window).load(function ()
+		{
+			action.steps[step]();
+			if (action.steps.length > step+1)
+				GM_setValue('step', step+1);
+			else
+			{
+				GM_deleteValue('step');
+				GM_deleteValue('action');
+			}
+		});
+	}
 }
-
