30c8151521b4eaafc42a3ecc9b7ccc5079b508e5
Timothy Johnson <timothy@idioticdev.com>
2017-03-20 17:10:55 -0700

    ii-duct-tape version 2. Reasign changed links. More efficient, ES6.

--- ii-duct-tape.user.js
+++ ii-duct-tape.user.js
@@ -1,130 +1,134 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name        ii-duct-tape
-// @namespace   http://RedHatter.github.com
+// @namespace   http://idioticdev.com
 // @description Makes the hotkeys more consistent, so that e.g. Bank of Improbable is always "b". 
 // @include     http://*improbableisland.com/*
 // @exclude     http://*improbableisland.com/home.php*
-// @version     1.0
+// @version     2
 // @grant       none
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @run-at      document-start
 // ==/UserScript==
-var keys = {
-	// universal_links
-	"Return to the Outpost"      : "o",
-	"View your Inventory"        : "i",
-	"Return to the Jungle"       : "j",
+let keys = {
+  // universal_links
+  "Return to the Outpost"      : "o",
+  "View your Inventory"        : "i",
+  "Return to the Jungle"       : "j",
 
-	// outpost_links
-	"eBoy's Trading Station"     : "e",
-	"Sheila's Shack O' Shiny"    : "s",
-	"Bank of Improbable"         : "b",
-	"Quit to the fields"         : "Q",
-	"New Day Menu"               : "*",
-	"The Hunter's Lodge"         : "l",
-	"Location Four"              : "4",
-	"Frequently Asked Questions" : "?",
-	"Council Offices"            : "c",
-	"Hall o' Fame"               : "f",
-	"Common Ground"              : "g",
-	"Clan Halls"                 : "h",
-	"Jungle"                     : "j",
-	"Daily News"                 : "n",
-	"Preferences"                : "p",
-	"Travel"                     : "t",
-	"List Contestants"           : "w",
-	"ScrapYard"                  : "#",
-	"Joe's Dojo"                 : "d",
-	"Rally Headquarters"         : "r",
-	"Reinforce the Defences"     : "r",
+  // outpost_links
+  "eBoy's Trading Station"     : "e",
+  "Sheila's Shack O' Shiny"    : "s",
+  "Bank of Improbable"         : "b",
+  "Quit to the fields"         : "Q",
+  "New Day Menu"               : "*",
+  "The Hunter's Lodge"         : "l",
+  "Location Four"              : "4",
+  "Frequently Asked Questions" : "?",
+  "Council Offices"            : "c",
+  "Hall o' Fame"               : "f",
+  "Common Ground"              : "g",
+  "Clan Halls"                 : "h",
+  "Jungle"                     : "j",
+  "Daily News"                 : "n",
+  "Preferences"                : "p",
+  "Travel"                     : "t",
+  "List Contestants"           : "w",
+  "ScrapYard"                  : "#",
+  "Joe's Dojo"                 : "d",
+  "Rally Headquarters"         : "r",
+  "Reinforce the Defences"     : "r",
 
-	// travel_links
-	"North"                      : "t",
-	"East"                       : "h",
-	"South"                      : "b",
-	"West"                       : "f",
-	"North-East"                 : "y",
-	"North-West"                 : "r",
-	"South-East"                 : "n",
-	"South-West"                 : "v",
-	"Enter the Jungle"           : "j",
-	"World Map"                  : "m",
+  // travel_links
+  "North"                      : "t",
+  "East"                       : "h",
+  "South"                      : "b",
+  "West"                       : "f",
+  "North-East"                 : "y",
+  "North-West"                 : "r",
+  "South-East"                 : "n",
+  "South-West"                 : "v",
+  "Enter the Jungle"           : "j",
+  "World Map"                  : "m",
 
-	// OST
-	"Go to AceHigh"              : "a",
-	"Go to Cyber City 404"       : "c",
-	"Go to Improbable Central"   : "i",
-	"Go to NewHome"              : "h",
-	
-	// Looking for trouble
-	"Hospital Tent"              : "h",
-	"Look for an Easy Fight"     : "1",
-	"Look for Trouble"           : "2",
-	"Look for Big Trouble"       : "3",
-	"Look for Really Big Trouble" : "4"
-	
-	// Down below
-	// "North" : "W",
-	// "South" : "S",
-	// "West" : "A",
-	// "East" : "D",
+  // OST
+  "Go to AceHigh"              : "a",
+  "Go to Cyber City 404"       : "c",
+  "Go to Improbable Central"   : "i",
+  "Go to NewHome"              : "h",
+  
+  // Looking for trouble
+  "Hospital Tent"              : "h",
+  "Look for an Easy Fight"     : "1",
+  "Look for Trouble"           : "2",
+  "Look for Big Trouble"       : "3",
+  "Look for Really Big Trouble" : "4"
+}
 
-	// ii-slaves
-	// "Heal" : "H"
-};
-
-waitForKeyElements ("script", function (node)
-{
+waitForKeyElements ("script", node => {
   unsafeWindow.document.onkeypress = function(e)
-	{
-		if ( e.altKey || e.ctrlKey || e.metaKey)
-			return;
-		
-		if ($('input:focus').length > 0)
-			return;
-		
-		var key = String.fromCharCode (e.charCode);
-		if (e.shiftKey)
-			key = key.toUpperCase ();
-		
-		$("[accesskey="+key+"]")[0].click ();
-	};
-});
+  {
+    if ( e.altKey || e.ctrlKey || e.metaKey)
+      return
+    
+    if ($('input:focus').length > 0) return
+    
+    let key = String.fromCharCode (e.charCode)
+    $(`[accesskey='${key}']`).eq(0).click()
+  }
+})
 
-waitForKeyElements ("a.nav", function (node)
-{
-	var title = $(node).text ();
-	if (title.charAt (0) == "(")
-		title = title.substring (4);
-	
-	var link = title.replace(new RegExp(" ?\\(.*?\\) ?", 'g'), "");
+let processed = []
+waitForKeyElements ("a.nav", node => {
+  if (processed.includes(node[0])) return
+  processed.push(node[0])
+  
+  let title = $(node).text()
+  if (title.charAt (0) == "(")
+    title = title.substring(4)
+  
+  let link = title.replace(new RegExp(" ?\\(.*?\\) ?", 'g'), "")
 
-	if (keys[link] == undefined)
-	{
-		var key = $(node).attr ("accesskey");
-		if (key == undefined)
-			return;
-		
-		key = key.toLowerCase ();
-		$(node).attr ("accesskey", key);
-		if ($("[accesskey='"+key+"']").length > 1)
-			$(node)
-				.text (title)
-				.attr ("accesskey", "");
-	} else
-	{
-		var key = keys[link];
-		var i = link.toLowerCase ().indexOf (key);
-		
-		$("[accesskey='"+key+"']").each (function( index )
-		{
-		$(this)
-				.text($(this).text ())
-				.attr ("accesskey", "");
-		});
-		$(node)
-			.html (i > -1 ? title.substring (0, i) + "<span class=\"navhi\">" + title.charAt (i) + "</span>" + title.substring (i+1) : "(<span class=\"navhi\">" + key + "</span>) " + title)
-			.attr ("accesskey", key);
-	}
-});
+  if (!keys[link]) {
+    let key = node.attr ("accesskey")
+    if (!key) return
+
+    assign(node, title, key.toLowerCase())
+  } else {
+    assign(node, title, keys[link])
+  }
+})
+
+let links = {}
+function assign (node, title, key) {
+  node.removeAttr('accesskey')
+  
+  if (!title) {
+    title = node.text()
+    if (title.charAt (0) == "(")
+      title = title.substring(4)
+  }
+  
+  if (!key)
+    for (let i = 0; i < title.length; i++) {
+      let tryk = title.charAt(i).toLowerCase()
+      if (links[tryk] || tryk == ' ' || tryk == "'") continue
+        
+      key = tryk
+      break
+    }
+    
+  if (!key) return
+
+  let dup = links[key]
+  links[key] = node
+  if (dup) assign(dup)
+
+  let i = title.toLowerCase().indexOf(key)
+  
+  $(node)
+    .html (i > -1
+      ? `${title.substring(0, i)}<span class="navhi">${title.charAt(i)}</span>${title.substring(i + 1)}`
+      : `(<span class="navhi">${key}</span>) ${title}`)
+    .attr ("accesskey", key)
+}
