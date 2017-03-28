29d60d505951f78f81276444bc4150a87af65a32
Timothy Johnson <timothy@idioticdev.com>
2017-03-28 16:10:25 -0700

    ii-sniffer: Complete re-write. Auto collects crates.

--- ii-sniffer.user.js
+++ ii-sniffer.user.js
@@ -2,89 +2,111 @@
 // @name        ii-sniffer
 // @namespace   http://idioticdev.com
 // @description Show sniffers range on map.
-// @include     http://*improbableisland.com/badnav.php*
 // @include     http://*improbableisland.com/inventory.php*
 // @include     http://*improbableisland.com/runmodule.php?module=worldmapen*
-// @version     2
+// @version     3
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_deleteValue
 // ==/UserScript==
+(function () {
+  let text = $('.content').text()
+  let match = text.match(/It displays, weakly, the number ([\d,]*) in dull red LED's before its radio module catches fire/)
+  if (match) {
+    reset()
+    GM_setValue('begin', true)
+    GM_setValue('totalCrates', parseInt(match[1]))
+    return
+  }
 
-let match = $('.content').text().match(/It displays, weakly, the number ([\d,]*) in dull red LED's before its radio module catches fire/)
-if (match) {
-  let player = JSON.parse(GM_getValue('player', '{}'))
-  GM_setValue('origin', JSON.stringify({
-    x: player.x - 3,
-    y: player.y - 3
-  }))
-  GM_setValue('totalCrates', parseInt(match[1]))
-  return
-}
+  let alt = $('[style*="images/map/marker_"]')
+  if (alt.length < 1) return
+  alt = alt.parents('td').attr('alt').match(/\((\d+), (\d+)\)/)
 
-let found = GM_getValue('foundCrates', 0)
-match = $('.content').text().match(/There are ([\d,]*) Supply Crates here/)
-if (match) {
-  found += parseInt(match[1])
-  GM_setValue('foundCrates', found)
-}
+  let player = {
+    x: alt[1],
+    y: alt[2]
+  }
 
-if ($('.content').text().includes('There is a Supply Crate here.'))
-  GM_setValue('foundCrates', ++found)
+  let cells = JSON.parse(GM_getValue('cells', '[]'))
+  if (GM_getValue('begin')) {
+    GM_deleteValue('begin')
 
-let attr = $('[style*="images/map/marker_"]').parents('td').attr('alt')
-if (!attr) return
+    for (let y = 0; y < 7; y++)
+      for (let x = 0; x < 7; x++)
+        cells.push({
+          x: player.x - 3 + x,
+          y: player.y - 3 + y
+        })
+        
+    GM_setValue('cells', JSON.stringify(cells))
+  }
 
-let split = attr.indexOf(',')
-let player = {
-  y: parseInt(attr.substring(1, split)),
-  x: parseInt(attr.substring(split + 2, attr.indexOf(')')))
-}
-GM_setValue('player', JSON.stringify(player))
+  if (cells.length < 1) {
+    reset()
+    return
+  }
 
-let origin = JSON.parse(GM_getValue('origin'))
-if (!origin) return
+  let cratesFound = GM_getValue('cratesFound', 0)
+  match = text.match(/There are ([\d,]*) Supply Crates here/)
+  if (match) {
+    let href = $('a:contains("Pick up all Supply Crates")').attr('href')
+    if (href) {
+      GM_setValue('cratesFound', cratesFound + parseInt(match[1]))
+      window.location = href
+      return
+    }
+  }
 
-$('.navhead').first().before(
-  $('<div/>', {
-    class: 'navhead',
-    text: 'Sniffer'
-  }),
-  $('<table/>').append(
-    $('<tr>').append($('<td/>').text('Total Crates '), $('<td/>').text(GM_getValue('totalCrates'))),
-    $('<tr>').append($('<td/>').text('Found Crates '), $('<td/>').text(found))
-  ),
-  $('<a/>', {
-    class: 'nav',
-    href: '',
-    text: 'Reset',
-    click: reset
-  }),
-  $('<br>').attr('clear', 'all')
-)
+  if (text.includes('There is a Supply Crate here.')) {
+    let href = $('a:contains("Pick up Supply Crate")').attr('href')
+    if (href) {
+      GM_setValue('cratesFound', ++cratesFound)
+      window.location = href
+      return
+    }
+  }
 
-let visited = `${GM_getValue('visited', '')}/${player.y},${player.x}`
-GM_setValue('visited', visited)
-
-let count = 0
-
-for (let y = 0; y < 7; y++)
-  for (let x = 0; x < 7; x++) {
-    if (visited.includes(`/${y + origin.y},${x + origin.x}`))
-      continue
+  cells.forEach((cell, i) => {
+    if (player.x == cell.x && player.y == cell.y) {
+      let cut = cells.slice(0)
+      cut.splice(i, 1)
+      GM_setValue('cells', JSON.stringify(cut))
+      return
+    }
     
-    $(`td [alt*="(${y + origin.y}, ${x + origin.x}"]`)
+    $(`td [alt*="(${cell.x}, ${cell.y}"]`)
       .css('opacity', '0.7')
       .parent().css('background-color', 'black')
-    count++
-  }
-  
-if (count < 1)
-  reset()
+  })
 
-function reset() {
-  GM_deleteValue('visited')
-  GM_deleteValue('origin')
-  $('td[style*="opacity: 0.7"]').css('opacity', 'initial')
-}
+  $('.navhead').first().before(
+    $('<div/>', {
+      class: 'navhead',
+      text: 'Sniffer'
+    }),
+    'You have found ',
+    $('<b>').text(cratesFound),
+    ' out of ',
+    $('<b>').text(GM_getValue('totalCrates', 0)),
+    ' supply crates.',
+    $('<a/>', {
+      class: 'nav',
+      href: '',
+      text: 'Reset',
+      click: e => {
+        e.preventDefault()
+        reset()
+      }
+    }),
+    $('<br>').attr('clear', 'all')
+  )
+
+  function reset() {
+    GM_deleteValue('cratesFound')
+    GM_deleteValue('totalCrates')
+    GM_deleteValue('cells')
+    $('td[style*="opacity: 0.7"]').css('opacity', 'initial')
+  }
+})()
