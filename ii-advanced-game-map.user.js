c2f3f1de00403be4e0d54f6adce7f53d8c0076ea
Timothy Johnson <timothy@idioticdev.com>
2017-03-30 17:51:26 -0700

    ii-advanced-game-map: Version 3

--- ii-advanced-game-map.user.js
+++ ii-advanced-game-map.user.js
@@ -1,56 +1,85 @@
 // ==UserScript==
 // @name        ii-advanced-game-map
-// @namespace   http://redhatter.gethub.com
-// @description Adds note taking and searching to the game map.
-// @include     http://*.improbableisland.com/*
-// @version     2
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
-// @require     http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js
+// @namespace   http://idioticdev.com
+// @include     http://*improbableisland.com/*
+// @version     3
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @grant       GM_setValue
 // @grant       GM_getValue
 // ==/UserScript==
-
-var FILTER = true;
-var SCALE = 1;
-var MARK_NOTES = true;
-
-var prv = new Array();
-
-var map = $("script[src = 'js/places_map\.js']").next();
-if (map.length)
-{
-	GM_setValue('map',map.html());
-
-	$('<input>',{
-		placeholder: 'Filter'
-	}).insertBefore(map)
-		.keyup(function (e)
-		{
-			var searchString = e.target.value;
-
-			$('[reset]').each(function ()
-			{
-				$(this).css('background-image',$(this).attr('reset'));
-			});
-
-
-			if (searchString.length < 2)
-				return;
-
-			prv = $("a[onclick]", map).filter(function ()
-			{
-				return $(this).attr('onclick').toLowerCase().indexOf(searchString) != -1;
-			}).parent().each(function ()
-			{
-				$(this).attr('reset',$(this).css('background-image'));
-			})
-			.css({'background-image': '', 'background-color': 'black'});
-		});
-} else
-{
-	$('<a/>',{text: 'Open map'}).click(function ()
-	{
-		$("<div><script type='text/javascript' src='js/places_map.js'></script><table border='0' cellpadding='0' cellspacing='0' width=645>"+GM_getValue('map')+"</table></div>")
-			.appendTo(document).dialog();
-	}).insertAfter("[href='http://merch.improbableisland.com']");
+let map = null
+let fulltext = {}
+if (window.location.href.includes('op=viewmap')) {
+  map = $('td[style*="images/map"]').closest('table').eq(0)
+  GM_setValue('map', map[0].outerHTML)
+  
+  let strip = $('<span>')
+  $('tr', map).each((y, tr) => {
+    $('td', tr).each((x, td) =>
+      $('a', td).each((i, a) => {
+        $(this).attr('title')
+        let text = $(a).attr('onclick')
+        text = text.substring(text.indexOf(','), text.lastIndexOf("'"))
+        text = text.replace(/\\'/g, '')
+        text = strip.html(text).text()
+        text = ($(a).attr('title') + text).toLowerCase()
+        fulltext[`${x},${y},${i}`] = text
+      }))
+  })
+  GM_setValue('fulltext', JSON.stringify(fulltext))
+} else {
+  fulltext = JSON.parse(GM_getValue('fulltext', '{}'))
+  $('<script>').attr('src', '/js/places_map.js').appendTo('head')
+  
+  map = $(GM_getValue('map')).css('margin', '50px auto')
+  let container = $('<div>').append(map)
+    .css({
+      height: 0,
+      transition: 'height 1s',
+      overflow: 'hidden'
+    })
+    .insertBefore($('.content h2').eq(0))
+  $('a:contains("MoTD")').before($('<a>', {
+    text: 'Map',
+    href: '',
+    click: e => {
+      e.preventDefault()
+      container.css('height', container.height() == 0 ? '1100px' : 0)
+    }
+  }), ' | ')
 }
+
+function includesAny(text, words) {
+  
+}
+
+function filter (text) {
+  let words = text.split(/\s+/)
+  let selected = {}
+  for (let id in fulltext)
+    if (words.every(word => fulltext[id].includes(word)))
+      selected[id] = fulltext[id]
+  
+  $('tr', map).each((y, tr) =>
+    $('td', tr).each((x, td) =>
+      $('a', td).each((i, a) => {
+        if (selected[`${x},${y - 1},${i}`])
+          $(a).show()
+        else
+          $(a).hide()
+      })))
+}
+
+let timer = null
+map.prepend(
+  $('<tr/>').append(
+    $('<td/>'),
+    $('<td/>', { colspan: 25 }).append(
+      $('<input>')
+        .keyup(function (e) {
+          window.clearTimeout(timer)
+          timer = window.setTimeout(() => filter($(this).val()), 100)
+        })
+        .css('width', '100%')
+        .attr('placeholder', 'Full text search...')
+      )))
