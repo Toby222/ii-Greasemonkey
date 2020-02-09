d9a16dca2d2798f004566394cb918dc95b8391da
toman222 <toman222@users.noreply.github.com>
2020-02-09 17:12:09 +0100

    Updated puzzle solver script to work again (#2)
    puzzle solver update

--- ii-puzzle-solver.user.js
+++ ii-puzzle-solver.user.js
@@ -2,7 +2,7 @@
 // @name        ii-puzzle-solver
 // @namespace   http://idioticdev.com
 // @description Solves for puzzle combat.
-// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @include     http://*improbableisland.com/*op=search*
 // @include     https://*improbableisland.com/*op=search*
 // @include     http://*improbableisland.com/*op=fight*
@@ -13,187 +13,248 @@
 // @include     https://*improbableisland.com/badnav.php*
 // @include     http://*improbableisland.com/runmodule.php?module=onslaught*
 // @include     https://*improbableisland.com/runmodule.php?module=onslaught*
-// @version     3.1
+// @version     4.0
 // @grant       GM_setValue
 // @grant       GM_getValue
-// @grant       GM_deleteValue
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
 
-/*
- * 1 = orange
- * 0 = green
- */
+let state = ''
+let broken = ''
+let zero = ''
+const inputs = []
+const details = $('<table/>')
 
-(function () {
-  let state = ''
-  let broken = ''
-  let zero = ''
-  let inputs = []
-  let details = $('<table/>')
+const log = () => {} // console.log //
 
-  if ($('a.nav > div > div').length < 1
-    || $('div[style*="puzzlecombat_red"]').length > 0)
+main()
+
+function main () {
+  log('ii-puzzle-solver main() start')
+  if ($('a.nav > div').length < 1) {
+    log('Not puzzle combat')
+    log('ii-puzzle-solver setup end')
     return
+  }
 
-  $(document).keypress (e => {
-    if (e.which > 48 && e.which < 58)
-      GM_setValue ('arm', e.which-48)
+  $(document).keypress(e => {
+    if (e.which > 48 && e.which < 58) {
+      GM_setValue('arm', e.which - 48)
+    }
   })
 
   setup()
   findMonster()
+  log('ii-puzzle-solver main end')
+}
 
-  function setup () {
-    let nextSection = $('div.navhead:contains("Indiscriminate Flailing")')
-    $('<div/>').addClass('navhead').text('Puzzle Solver').insertBefore(nextSection)
+function setup () {
+  log('ii-puzzle-solver setup() start')
+  const nextSection = $('div.navhead')[0]
+  $('<div/>').addClass('navhead').text('Puzzle Solver').insertBefore(nextSection)
 
-    $('a.nav > div > div').each((i, el) => {
-      let bg = $(el).css ("background-image")
-      state += bg.indexOf ('orange') !== -1 ? '1' : '0'
-      broken += bg.indexOf ('disabled') !== -1 ? '1' : '0'
-      zero += '0'
+  $('a.nav > div').each((i, el) => {
+    el = el.parentElement
 
-      inputs.push($('<input/>')
-        .css({
-          margin: '5px 0 0 8px',
-          width: $(el).width() + 'px',
-          background: 'none',
-          border: 'none',
-          'border-bottom': '1px solid black'
-        })
-        .insertBefore(nextSection))
-    })
-
-    $('<a>').text('Solve Puzzle')
-      .attr('href', '')
-      .addClass('nav')
-      .css('padding', '8px')
-      .click(e => {
-        e.preventDefault()
-        parsePuzzle()
-      })
-      .insertBefore(nextSection)
-
-    // Display information
-    let previous = GM_getValue ('previous')
-    let diffBinary = parseInt(previous, 2) ^ parseInt(state, 2)
-    let differance = [];
-    for (let i = 0; i < state.length; i++) {
-      let mask = 1 << i;
-      if ((diffBinary & mask) == mask)
-        differance.push(state.length - i)
+    let status
+    if (el.innerText.split('\n').slice(-2)[0].includes('Guarded')) {
+      status = 'guarded'
+    } else if (el.innerText.split('\n').slice(-2)[0].includes('Disabled')) {
+      status = 'disabled'
+    } else if (el.innerText.split('\n').slice(-2)[0].includes('Vulnerable') || el.innerText.split('\n').slice(-2)[0].includes('Exposed')) {
+      status = 'vulnerable'
+    } else if (el.innerText.split('\n').slice(-2)[0].includes('Hidden')) {
+      status = 'hidden'
+    } else {
+      console.error(el.children)
+      console.error('Unknown status: ' + el.innerText.split('\n').slice(-2)[0])
     }
 
-    details.append(
-      $('<tr/>').append($('<td/>').text('Current'), $('<td/>').text(state)),
-      $('<tr/>').append($('<td/>').text('Previous'), $('<td/>').text(previous)),
-      $('<tr/>').append($('<td/>').text('Differance'), $('<td/>').text(differance.reverse().join(','))),
-      $('<tr/>').append($('<td/>').text('Arm Attacked'), $('<td/>').text(GM_getValue('arm')))
-    ).insertBefore(nextSection)
-    GM_setValue('previous', state)
-  }
-
-  function findMonster () {
-    let monster = $('#combatbars table:last td:last').text()
-    if (monster.indexOf ('Elite') == 0) monster = monster.slice (6)
-    else if (monster.indexOf ('Deadly') == 0) monster = monster.slice (7)
-    else if (monster.indexOf ('Lethal') == 0) monster = monster.slice (7)
-    else if (monster.indexOf ('Savage') == 0) monster = monster.slice (7)
-    else if (monster.indexOf ('Malignant') == 0) monster = monster.slice (10)
-    else if (monster.indexOf ('Dangerous') == 0) monster = monster.slice (10)
-    else if (monster.indexOf ('Malevolent') == 0) monster = monster.slice (11)
-
-    let count = $(`#combatbars table b:contains("${monster}")`).length
-    if (count > 1)
-      monster = `${monster} (x${count})`
-
-    let level = parseInt($('.content span:contains("(Level ")').text().substring(7))
-
-    if (parseInt(state, 2) === 0)
-      retrieveStun (monster, level)
-    else
-      retrieveTargets (monster, level)
-  }
-
-  function retrieveTargets (name, level) {
-    querySpreadsheet(name === 'Lion' ?
-      `select C,D,E,F,G,H,I,J,K where A contains "Lion (${level})" and B=${level}` :
-      `select C,D,E,F,G,H,I,J,K where A="${name}"`, targets => {
-        inputs.forEach((input, i) => input.val(targets[i] ? targets[i].v : ''))
-        parsePuzzle()
-      })
-  }
-
-  function retrieveStun (name, level) {
-    querySpreadsheet(name === 'Lion' ?
-      `select L,M,N,O,P where A contains "Lion (${level})" and B=${level}` :
-      `select L,M,N,O,P where A="${name}"`, sequence => {
-        $('<tr/>').append(
-          $('<td/>').text('Stun Sequence'),
-          $('<td/>').text(sequence.map(c => c.v))
-        ).appendTo(details)
-      })
-  }
-
-  function querySpreadsheet (query, callback) {
-    GM_xmlhttpRequest ({
-      method: "GET",
-      url: `https://spreadsheets.google.com/tq?tq=${query}&key=0AtPkUdingtHEdFUzLWN0a3dkNDlyT09HNjVsNjg2ZXc`,
-      onload: response => {
-        callback(JSON.parse (
-          response.responseText.slice (
-            response.responseText.indexOf('(') + 1,
-            response.responseText.length-2)).table.rows[0].c)
-      }
-    })
-  }
-
-  function parsePuzzle () {
-    let toggles = [];
-    for (let i = 0; i < state.length; i++) {
-      let toggle = inputs[i].val().split (',')
-      let binary = zero
-      for (let a of toggle) {
-        let index = parseInt(a) - 1
-        binary = binary.substring(0, index) + '1' + binary.substr(index + 1)
-      }
-
-      toggles.push (parseInt (binary, 2))
+    switch (status) {
+      case 'guarded':
+        state += '1'
+        broken += '0'
+        break
+      case 'disabled':
+        state += '0'
+        broken += '1'
+        break
+      case 'vulnerable':
+        state += '0'
+        broken += '0'
+        break
+      case 'hidden':
+        state += '?'
+        broken += '0'
     }
 
-    let result = solve(toggles, parseInt(state, 2), parseInt(broken, 2))
-    for (let arm of result)
-      inputs[arm - 1].css('background-color', 'green')
+    zero += '0'
+    inputs.push($('<input/>')
+      .css({
+        margin: '5px 0 0 8px',
+        width: $(el).width() + 'px',
+        background: 'none',
+        border: 'none',
+        'border-bottom': '1px solid black'
+      }))
+  })
+
+  if (parseInt(state) !== 0) {
+    for (const input of inputs) {
+      input.insertBefore(nextSection)
+    }
   }
 
-  function solve (array, test, filter) {
-    var result = [];
+  // Display information
+  const previous = GM_getValue('previous')
+  const diffBinary = parseInt(previous, 2) ^ parseInt(state, 2)
+  const difference = []
+  for (let i = 0; i < state.length; i++) {
+    const mask = 1 << i
+    if ((diffBinary & mask) === mask) {
+      difference.push(state.length - i)
+    }
+  }
 
-    function loop (start,depth,prefix) {
-      for (var i=start; i<array.length; i++) {
-        var next = prefix^array[i];
-        if (depth > 0) {
-          if (loop (i+1,depth-1,next)) {
-            result.push (i+1);
-            return true;
-          }
-        } else {
-          var combo = test^next;
-          if (combo == 0 || (combo&filter) == combo) {
-            result.push (i+1);
-            return true;
-          }
+  details.append(
+    $('<tr/>').append($('<td/>').text('Current:'), $('<td/>').text(state)),
+    $('<tr/>').append($('<td/>').text('Previous:'), $('<td/>').text(previous)),
+    $('<tr/>').append($('<td/>').text('Difference:'), $('<td/>').text(difference.reverse().join(','))),
+    $('<tr/>').append($('<td/>').text('Arm Attacked:'), $('<td/>').text(GM_getValue('arm')))
+  ).insertBefore(nextSection)
+  GM_setValue('previous', state)
+  log('ii-puzzle-solver setup end')
+}
+
+function findMonster () {
+  log('ii-puzzle-solver findMonster() start')
+  let monster = $('span.colLtYellow')[0].textContent
+
+  // Is this theme-dependant?
+  const count = $('td.content > span > span.colLtRed').length - 1
+  if (count > 1) {
+    monster = `${monster} (x${count})`
+  }
+
+  const level = parseInt($('.content span:contains("(Level ")').text().substring(7))
+
+  if (parseInt(state, 2) === 0) {
+    retrieveStun(monster, level)
+  } else {
+    retrieveTargets(monster, level)
+  }
+  log('ii-puzzle-solver findMonster end')
+}
+
+function retrieveTargets (name, level) {
+  log(`ii-puzzle-solver retrieveTargets({${name}}, {${level}}) start`)
+  querySpreadsheet(name === 'Lion' ? `select C,D,E,F,G,H,I,J,K where A contains "Lion (${level})" and B=${level}` : `select C,D,E,F,G,H,I,J,K where A="${name}"`,
+    response => {
+      // log(response.responseText.slice(response.responseText.indexOf('(') + 1, response.responseText.length - 2))
+      const targets = JSON.parse(response.responseText.slice(response.responseText.indexOf('(') + 1, response.responseText.length - 2)).table.rows[0].c
+      inputs.forEach((input, i) => input.val(targets[i] ? targets[i].v : ''))
+      parsePuzzle()
+    }
+  )
+  log('ii-puzzle-solver retrieveTargets end')
+}
+
+function retrieveStun (name, level) {
+  log(`ii-puzzle-solver retrieveStun({${name}}, {${level}})) start`)
+  querySpreadsheet(name === 'Lion' ? `select L,M,N,O,P where A contains "Lion (${level})" and B=${level}` : `select L,M,N,O,P where A="${name}"`,
+    response => {
+      log(response.responseText.slice(response.responseText.indexOf('(') + 1, response.responseText.length - 2))
+      const sequence = JSON.parse(response.responseText.slice(response.responseText.indexOf('(') + 1, response.responseText.length - 2)).table.rows[0].c
+      $('<tr/>').append(
+        $('<td/>').text('Stun Sequence'),
+        $('<td/>').text(sequence.map(c => c.v))
+      ).appendTo(details)
+    })
+  log('ii-puzzle-solver retrieveStun end')
+}
+
+function querySpreadsheet (query, callback) {
+  log(`ii-puzzle-solver querySpreadsheet({${query}}, {${callback.valueOf()}}) start`)
+  query = encodeURIComponent(query)
+  // log(`URL: https://docs.google.com/spreadsheets/d/19M0BP55bQPHCodKc-KXQzC_zBKwVa5PXHUAKLvwdDaE/gviz/tq?tq=${query}`)
+  GM_xmlhttpRequest({
+    method: 'GET',
+    url: `https://docs.google.com/spreadsheets/d/19M0BP55bQPHCodKc-KXQzC_zBKwVa5PXHUAKLvwdDaE/gviz/tq?tq=${query}`,
+    onload: response => {
+      callback(response)
+    }
+  })
+  log('ii-puzzle-solver querySpreadsheet end')
+}
+
+function parsePuzzle () {
+  log('ii-puzzle-solver parsePuzzle() start')
+  const toggles = []
+  for (const input of inputs) {
+    let binary = '0'.repeat(inputs.length)
+    for (const a of input.val().split(',')) {
+      binary = binary.substring(0, a - 1) + '1' + binary.substr(a)
+    }
+
+    toggles.push(parseInt(binary, 2))
+  }
+
+  const result = solve(toggles, parseInt(state, 2), parseInt(broken, 2))
+  for (const arm of result) {
+    inputs[arm - 1].css('background-color', 'green')
+  }
+  log('ii-puzzle-solver parsePuzzle end')
+}
+
+/*
+```
+you have some number X
+as well as two lists of numbers
+the first list L1 contains numbers you can XOR with X
+the second list L2 contains the indices of X's binary representation that will always be 0
+
+XOR X with any set of the numbers, in any order, as often as you want, so that X becomes 0
+```
+*/
+
+// array: list of toggles of each limb
+// test: current monster state
+// filter: broken limbs
+
+function solve (array, test, filter) {
+  // [225,77,161,180,90,46,110,37],
+  // 45,
+  // 0
+
+  log(`ii-puzzle-solver solve([${array}], 0b${test.toString(2)}, 0b${filter.toString(2)}) start`)
+
+  const result = []
+
+  function loop (start, depth, prefix) {
+    for (var i = start; i < array.length; i++) {
+      var next = prefix ^ array[i]
+      if (depth > 0) {
+        if (loop(i + 1, depth - 1, next)) {
+          result.push(i + 1)
+          return true
+        }
+      } else {
+        var combo = test ^ next
+        if (combo === 0 || (combo & filter) === combo) {
+          result.push(i + 1)
+          return true
         }
       }
-
-      return false;
     }
 
-    for (var i=0; i<array.length; i++)
-      if (loop (0,i,0))
-        break;
-
-    return result;
+    return false
   }
-})()
+
+  for (var i = 0; i < array.length; i++) {
+    if (loop(0, i, 0)) {
+      break
+    }
+  }
+  log(`ii-puzzle-solver solve end [result: {${result}}]`)
+  return result
+}
